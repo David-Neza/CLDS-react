bebe75966833d4f59340769f2d46806f6f4c5fb6
neza <tuyishmirend@gmail.com>
2022-12-01 16:09:16 +0200

    adding projects

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,64 +3,64 @@ import "../Components/Modal.css"
 import "../App.css";
 import image from "../images/david.jpg"
 import Navbar from "./Navbar";
-
+import { Popover } from 'antd';
+import project1 from "../images/bbcrweb.png"
 
 function HomePage(){ 
-    
+  const content = (
+    <div>
+      <p className="tex-red-400">Contact me on Whatsapp : 0781636072</p>
+      <p>Message me on <a href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+    </div>
+  );
       return (
         <>
           <div>
             <Navbar />
-            <div className="pt-4 pl-36">
+            <div className="pt-16 pl-20">
             <div className="flex space-x-24">
               <div className="">
-                <img class="object-cover h-[300px] w-[500px] rounded" src={image}/>
+                <img class="object-cover h-[600px] w-[700px] rounded" src={image}/>
               </div>
 
-              <div className="text-black-300 font-bold p-12 text-xl">
-                  <p className="flex flex-start text-6xl">Hi there!</p>
-                  <p className="text-gray-400">My passion is to use my software engineering skills to make it easy <br/>for people to access services that are essential to them.  To achieve <br/>that  I work hard and study relentlesly in order to keep up with the fast <br/>growing technology industry</p>
-              </div>
+              <div className="grid text-black-300 font-bold text-xl pt-48 place-items-start">
+                  <p className="flex flex-start text-3xl">Hi there!</p>
+                  <p className="flex flex-start font-[1500] text-6xl">I am David Neza Tuyishimire <br/>
+                  A software Engineer based in <br/> Kigali, Rwanda</p>
+                  <Popover content={content}>
+                  <p className="border-2 border-red-300 w-28 text-red-300 font-bold"> Say Hello!</p>
+             
+  </Popover>
+            </div>
               </div>
             </div>
-            <div className="flex flex-start pt-24 pl-32 space-x-24">
-            <div className="text-gray-400 text-xl">
-                <div>
-                <p className="flex flex-start text-black text-2xl">My career So Far</p>
-                <p>
-                  Always up for a challenge I was worked on many projects. Noticably a bus tracking that<br/>
-                  that allowed users to know when the next bus will arrive at their bus stop. I also worked<br/>
-                  on a web app that kept records of construction workers attendances and provided  them<br/>
-                  payments to them at the end of two weeks
-                </p>
-                </div>
+          <div className="pt-10">
+            <h1 className="font-bold text-6xl">Latest Works</h1>
+            <div className="flex space-x-24 pl-48">
+            <div className="">
+                <img class="object-cover h-[300px] w-[700px] rounded" src={project1}/>
+              </div>
+
+              <div className="grid place-items-start">
+              <h1 className="font-bold text-3xl">Bibleway Baptist Church website</h1>
+              <p>This is a website I create for my church You can visit this website to go <br/>
+              the website and learn more about the church</p>
+              </div>
             </div>
-            <div className="pl-24">
-              <div className="flex space-x-4 font-bold">
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">Javascript</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React Native</p>
+          </div>
+          <div className="pt-10">
+            <div className="flex space-x-24 pl-48">
+            <div className="">
+                <img class="object-cover h-[300px] w-[700px] rounded" src={project1}/>
               </div>
-              <div className="flex space-x-4 font-bold">
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">Javascript</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React Native</p>
+
+              <div className="grid place-items-start">
+              <h1 className="font-bold text-3xl">Bibleway Baptist Church website</h1>
+              <p>This is a website I create for my church You can visit this website to go <br/>
+              the website and learn more about the church</p>
               </div>
-              <div className="flex space-x-4 font-bold">
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">Javascript</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React Native</p>
-              </div>
-              <div className="flex space-x-4 font-bold">
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">Javascript</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400">React</p>
-                <p className="bg-gray-100 p-2 rounded border w-36 border-gray-400 font-bold">React Native</p>
-              </div>
-              
             </div>
-                
-               
-            </div>
+          </div>
           </div>
         </>
     );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 const Navbar = () => {
     return ( 
         <>
-                <div className="p-4">
+                <div className="pt-8 pl-20 pr-20">
                 <div className=" font-Roboto text-xl flex justify-between mx-auto">
-                    <p className="text-extrabold font-Roboto">David Neza Tuyishimire</p>
-                    <div className="flex font-bold space-x-12">
-                    <p>LinkedIn</p>
+                   <div className="flex font-bold space-x-12">
+                    <p>Home</p>
+                    <a href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                     <p>Contact Me</p>
                     <p className="bg-black text-white text-bold w-28 h-8 rounded">View Work</p>
                     </div>
+                    <p className="text-black font-extrabold">David Neza Tuyishimire</p>
+                    <p className="border-2 border-red-300 w-28 text-red-300 font-bold">Hire Me > </p>
+                    
                 </div>
                 </div>  
         </>

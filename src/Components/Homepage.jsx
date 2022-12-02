99cd9fe06244923db1054aeb8b732e1e7097f848
neza <tuyishmirend@gmail.com>
2022-12-02 10:47:32 +0200

    view work section

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,10 +1,9 @@
-
-import "../Components/Modal.css"
+import "../Components/Modal.css";
 import "../App.css";
-import image from "../images/david.jpg"
+import image from "../images/david.jpg";
 import Navbar from "./Navbar";
-import { Popover } from 'antd';
-import project1 from "../images/bbcrweb.png"
+import { Popover } from "antd";
+import project1 from "../images/bbcrweb.png";
 import {
   FaLinkedinIn,
   FaGithub,
@@ -13,81 +12,157 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 
-function HomePage(){ 
+function HomePage() {
   const content = (
     <div>
       <p className="tex-red-400">Contact me on Whatsapp : 0781636072</p>
-      <p>Message me on <a href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p>
+        Message me on{" "}
+        <a
+          href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
     </div>
   );
-      return (
-        <>
-          <div>
-            <Navbar />
-            <div className="pt-16 pl-20">
-            <div className="flex space-x-24">
-              <div className="">
-                <img class="object-cover h-[600px] w-[700px] rounded" src={image}/>
-              </div>
-
-              <div className="grid text-black-300 font-bold text-xl pt-48 place-items-start">
-                  <p className="flex flex-start text-3xl">Hi there!</p>
-                  <p className="flex flex-start font-[1500] text-6xl">I am David Neza Tuyishimire <br/>
-                  A software Engineer based in <br/> Kigali, Rwanda</p>
-                  <Popover content={content}>
-                  <p className="border-2 border-red-300 w-28 text-red-300 font-bold"> Say Hello!</p>
-             
-  </Popover>
-            </div>
-              </div>
-            </div>
-          <div className="pt-10">
-            <h1 className="font-bold text-6xl">Latest Works</h1>
-            <div className="flex space-x-24 pl-48">
+  const hoverOnWork = (
+    <div>
+      <p className="tex-red-400">Click to view work</p>
+    </div>
+  );
+  return (
+    <>
+      <div>
+        <Navbar />
+        <div className="pt-16 pl-20">
+          <div className="flex space-x-24">
             <div className="">
-                <img class="object-cover h-[300px] w-[700px] rounded" src={project1}/>
-              </div>
+              <img
+                class="object-cover h-[600px] w-[700px] rounded"
+                src={image}
+              />
+            </div>
 
-              <div className="grid place-items-start">
-              <h1 className="font-bold text-3xl">Bibleway Baptist Church website</h1>
-              <p>This is a website I create for my church You can visit this website to go <br/>
-              the website and learn more about the church</p>
-              </div>
+            <div className="grid text-black-300 font-bold text-xl pt-48 place-items-start">
+              <p className="flex flex-start text-3xl">Hi there!</p>
+              <p className="flex flex-start font-[1500] text-6xl">
+                I am David Neza Tuyishimire <br />
+                A software Engineer based in <br /> Kigali, Rwanda
+              </p>
+              <Popover content={content}>
+                <p className="border-2 border-red-300 w-28 text-red-300 font-bold">
+                  {" "}
+                  Say Hello!
+                </p>
+              </Popover>
             </div>
           </div>
-          <div className="pt-10">
-            <div className="flex space-x-24 pl-48">
-            <div className="">
-                <img class="object-cover h-[300px] w-[700px] rounded" src={project1}/>
-              </div>
-
-              <div className="grid place-items-start">
-              <h1 className="font-bold text-3xl">Bibleway Baptist Church website</h1>
-              <p>This is a website I create for my church You can visit this website to go <br/>
-              the website and learn more about the church</p>
-              </div>
-            </div>
-          </div>
-        <div className="mt-8">
-        <footer class="p-4 bg-red-400 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-xl text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">David Neza Tuyishimire™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-xl text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">Home</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">LinkedIn</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Contact Me</a>
-        </li>
-    </ul>
-</footer>
         </div>
+        <div className="mt-16">
+          <h1 className="font-bold text-6xl">Latest Works</h1>
+          <Popover content={hoverOnWork}>
+            <a
+              className="hover:no-underline
+            text-blue-600 hover:text-blue-800 
+            visited:text-black"
+              href="https://bibleway-baptist-churches-of-rwanda.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="ml-[400px] w-[1000px] drop-shadow-2xl hover:bg-[#eff6ff] rounded">
+                <div className="flex space-x-24">
+                  <div className="">
+                    <img
+                      class="object-cover h-[300px] w-[700px]"
+                      src={project1}
+                    />
+                  </div>
+
+                  <div className="grid place-items-start">
+                    <h1 className="font-bold text-2xl pr-8">
+                      Bibleway Baptist Church website
+                    </h1>
+                    <p>
+                      This is a website I create for my church You can visit
+                      this website to go <br />
+                      the website and learn more about the church
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Popover>
+
+          <div className="mt-16">
+            <Popover content={hoverOnWork}>
+              <a
+                className="hover:no-underline
+            text-blue-600 hover:text-blue-800 
+            visited:text-black"
+                href="https://bibleway-baptist-churches-of-rwanda.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="ml-[400px] w-[1000px] drop-shadow-2xl hover:bg-[#eff6ff] rounded">
+                  <div className="flex space-x-24">
+                    <div className="">
+                      <img
+                        class="object-cover h-[300px] w-[700px]"
+                        src={project1}
+                      />
+                    </div>
+
+                    <div className="grid place-items-start">
+                      <h1 className="font-bold text-2xl pr-8">
+                        Bibleway Baptist Church website
+                      </h1>
+                      <p>
+                        This is a website I create for my church You can visit
+                        this website to go <br />
+                        the website and learn more about the church
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </Popover>
           </div>
-        </>
-    );
-  }
-  
-  export default HomePage;
+        </div>
+
+        <div className="mt-8">
+          <footer class="p-4 bg-red-400 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span class="text-xl text-gray-500 sm:text-center dark:text-gray-400">
+              © 2022{" "}
+              <a href="https://flowbite.com/" class="hover:underline">
+                David Neza Tuyishimire™
+              </a>
+              . All Rights Reserved.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-xl text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">
+                  Contact Me
+                </a>
+              </li>
+            </ul>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,33 @@
 const Navbar = () => {
-    return ( 
-        <>
-                <div className="pt-8 pl-20 pr-20">
-                <div className=" font-Roboto text-xl flex justify-between mx-auto">
-                   <div className="flex font-bold space-x-12">
-                    <p>Home</p>
-                    <a href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                    <p>Contact Me</p>
-                    <p className="bg-black text-white text-bold w-28 h-8 rounded">View Work</p>
-                    </div>
-                    <p className="text-black font-extrabold">David Neza Tuyishimire</p>
-                    <p className="border-2 border-red-300 w-28 text-red-300 font-bold">Hire Me > </p>
-                    
-                </div>
-                </div>  
-        </>
-     );
-}
- 
+  return (
+    <>
+      <div className="pt-8 pl-20 pr-20">
+        <div className=" font-Roboto text-xl flex justify-between mx-auto">
+          <div className="flex font-bold space-x-12">
+            <p>Home</p>
+            <a
+              className="hover:no-underline
+                    text-blue-600 hover:text-blue-800 
+                    visited:text-black"
+              href="https://www.linkedin.com/in/david-neza-tuyishimire-510845225/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <p>Contact Me</p>
+            <p className="bg-black text-white text-bold w-28 h-8 rounded">
+              View Work
+            </p>
+          </div>
+          <p className="text-black font-extrabold">David Neza Tuyishimire</p>
+          <p className="border-2 border-red-300 w-28 text-red-300 font-bold">
+            Hire Me >{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Navbar;
